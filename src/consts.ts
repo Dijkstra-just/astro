bf8cbbb5d565e800fcaef743788e3307abee9729
Dijkstra <396310954@qq.com>
2024-12-31 19:00:42 +0800

    xiugailezhuyemian

--- src/consts.ts
+++ src/consts.ts
@@ -5,13 +5,13 @@
 export const SITE_LANG = "en";
 export const SITE_TAB = "Frosti";
 export const SITE_TITLE = "Frosti 🧊";
-export const SITE_DESCRIPTION = "A blog template";
+export const SITE_DESCRIPTION = "Dijkstra's template";
 export const DATE_FORMAT = "ddd MMM DD YYYY";
 
 // User profile information
-export const USER_NAME = "EveSunMaple";
-export const USER_SITE = "https://frosti.saroprock.com"
-export const USER_AVATAR = "/profile.webp";
+export const USER_NAME = "Dijkstra";
+export const USER_SITE = ""
+export const USER_AVATAR = "/jhd.jpg";
 
 // Server and transition settings
 export const SERVER_URL = "https://demo.saroprock.com";
